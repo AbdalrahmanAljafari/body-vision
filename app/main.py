1e742ec09c5c91156c8ffbbf78225d1928708354
AbdalrahmanAljafari <aljafariabdelrahman@gmail.com>
2026-03-11 21:45:03 +0300

    add root endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.api.routes.standardization import router as standardization_router
 
-app = FastAPI(title="Body Image Standardization API")
+app = FastAPI(title="Body Vision API")
 
 app.include_router(standardization_router)
 
