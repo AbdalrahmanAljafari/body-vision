9f72d7723d57ef71a0625a38cedb5c72dfdcf552
AbdalrahmanAljafari <aljafariabdelrahman@gmail.com>
2026-03-11 21:44:07 +0300

    add root endpoint

--- app/main.py
+++ app/main.py
@@ -4,3 +4,7 @@ from app.api.routes.standardization import router as standardization_router
 app = FastAPI(title="Body Image Standardization API")
 
 app.include_router(standardization_router)
+
+@app.get("/")
+def root():
+    return {"message": "Body Vision API is running"}
